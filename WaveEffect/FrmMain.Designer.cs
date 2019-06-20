0414d0c7478229a485063d6e512220ff1b42ea1b
guoxinyou <guoxinyou@redinfo.cn>
2019-06-20 15:01:35 +0800

    初始化

---
+++ WaveEffect/FrmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WaveEffect
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWavelength = new System.Windows.Forms.TrackBar();
+            this.tbAmplitude = new System.Windows.Forms.TrackBar();
+            this.lblWaveLength = new System.Windows.Forms.Label();
+            this.lblAmplitude = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbWavelength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbAmplitude)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbWavelength
+            // 
+            this.tbWavelength.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbWavelength.Location = new System.Drawing.Point(137, 0);
+            this.tbWavelength.Maximum = 100;
+            this.tbWavelength.Minimum = 1;
+            this.tbWavelength.Name = "tbWavelength";
+            this.tbWavelength.Size = new System.Drawing.Size(862, 45);
+            this.tbWavelength.TabIndex = 3;
+            this.tbWavelength.Value = 1;
+            this.tbWavelength.Scroll += new System.EventHandler(this.tbWavelength_Scroll);
+            // 
+            // tbAmplitude
+            // 
+            this.tbAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAmplitude.Location = new System.Drawing.Point(137, 51);
+            this.tbAmplitude.Maximum = 100;
+            this.tbAmplitude.Minimum = 1;
+            this.tbAmplitude.Name = "tbAmplitude";
+            this.tbAmplitude.Size = new System.Drawing.Size(862, 45);
+            this.tbAmplitude.TabIndex = 5;
+            this.tbAmplitude.Value = 2;
+            this.tbAmplitude.Scroll += new System.EventHandler(this.tbAmplitude_Scroll);
+            // 
+            // lblWaveLength
+            // 
+            this.lblWaveLength.Location = new System.Drawing.Point(31, 0);
+            this.lblWaveLength.Name = "lblWaveLength";
+            this.lblWaveLength.Size = new System.Drawing.Size(100, 23);
+            this.lblWaveLength.TabIndex = 6;
+            this.lblWaveLength.Text = "波长";
+            this.lblWaveLength.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblAmplitude
+            // 
+            this.lblAmplitude.Location = new System.Drawing.Point(31, 51);
+            this.lblAmplitude.Name = "lblAmplitude";
+            this.lblAmplitude.Size = new System.Drawing.Size(100, 23);
+            this.lblAmplitude.TabIndex = 8;
+            this.lblAmplitude.Text = "振幅";
+            this.lblAmplitude.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 450);
+            this.Controls.Add(this.lblAmplitude);
+            this.Controls.Add(this.lblWaveLength);
+            this.Controls.Add(this.tbAmplitude);
+            this.Controls.Add(this.tbWavelength);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.tbWavelength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbAmplitude)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar tbWavelength;
+        private System.Windows.Forms.TrackBar tbAmplitude;
+        private System.Windows.Forms.Label lblWaveLength;
+        private System.Windows.Forms.Label lblAmplitude;
+    }
+}
+
---
+++ WaveEffect/FrmMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaveEffect
+{
+    public partial class FrmMain : Form
+    {
+        private Graphics formGraphics;
+
+        private Bitmap bitmap;
+
+        private Bitmap sourceMap;
+
+        private byte[] sourceArray;
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.Shown += FrmMain_Shown;
+        }
+
+        private void FrmMain_Shown(object sender, EventArgs e)
+        {
+            formGraphics = this.CreateGraphics();
+            sourceMap = global::WaveEffect.Properties.Resources.img6;
+            bitmap = new Bitmap(sourceMap.Width, sourceMap.Height);
+            Rectangle rect = new Rectangle(0, 0, sourceMap.Width, sourceMap.Height);
+            System.Drawing.Imaging.BitmapData bmpData = sourceMap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr ptr = bmpData.Scan0;
+            int bytesCount = bmpData.Stride * bmpData.Height;
+            sourceArray = new byte[bytesCount];
+            Marshal.Copy(ptr, sourceArray, 0, bytesCount);
+            sourceMap.UnlockBits(bmpData);
+        }
+
+        private void tbWavelength_Scroll(object sender, EventArgs e)
+        {
+            this.Render();
+        }
+        private void tbAmplitude_Scroll(object sender, EventArgs e)
+        {
+            this.Render();
+        }
+
+        private void Render()
+        {
+            double rate = this.tbWavelength.Value;
+            double ratio = this.tbAmplitude.Value * 0.01;
+
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);//curBitmap.PixelFormat
+            IntPtr ptr = bmpData.Scan0;
+            int bytesCount = bmpData.Stride * height;
+            byte[] arrDst = new byte[sourceArray.Length];
+            Array.Copy(sourceArray, arrDst, sourceArray.Length);
+            for (int i = 0; i < bytesCount; i += 3)
+            {
+                int y = (int)(i / bmpData.Stride) - height / 2;
+                int x = (i % bmpData.Stride) / 3 - width / 2;
+                double p = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                int tranx = (int)(x + x * ratio * Math.Sin(p / rate));
+                int trany = (int)(y + y * ratio * Math.Sin(p / rate));
+                int pixx = tranx + width / 2;
+                int pixy = trany + height / 2;
+
+                if (pixx > 0 && pixx < width && pixy > 0 && pixy < height)
+                {
+                    arrDst[pixx * 3 + pixy * bmpData.Stride] = sourceArray[i];
+                    arrDst[pixx * 3 + pixy * bmpData.Stride + 1] = sourceArray[i + 1];
+                    arrDst[pixx * 3 + pixy * bmpData.Stride + 2] = sourceArray[i + 2];
+                }
+            }
+            Marshal.Copy(arrDst, 0, ptr, arrDst.Length);
+            bitmap.UnlockBits(bmpData);
+            formGraphics.DrawImage(bitmap, 100, 100);
+        }
+
+
+    }
+}
